7a254b7a1333f09b62a83a887db44da70c294b3d
Nandang Hermawan <nandanghermawan@Nandangs-MacBook-Pro.local>
2020-01-17 22:52:56 +0700

    first commit

---
+++ lib/presenter/LoginPresenter.dart
@@ -0,0 +1,143 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:pelatihan_dasar_flutter/model/userModel.dart';
+import 'package:pelatihan_dasar_flutter/view/LoginView.dart';
+
+import '../main.dart';
+
+class Login extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return LoginView();
+  }
+}
+
+abstract class LoginPresenter extends State<Login> {
+  bool obscureText = true;
+  Color borderUsernameColor = Colors.white;
+  Color borderPasswordColor = Colors.white;
+  bool onLoading = false;
+
+  TextEditingController userNameController = new TextEditingController();
+  TextEditingController passwordController = new TextEditingController();
+
+  FocusNode usernameFocusNode = FocusNode();
+  FocusNode passwordFocusNode = FocusNode();
+
+  UserModel user;
+
+  @override
+  void initState() {
+    super.initState();
+    //get user from pref
+    String _user = (prefs.getString(PrefsKey.user));
+    print(_user);
+    user = _user == null ? null : UserModel.fromJson(json.decode(_user));
+  }
+
+  void openObscureText() {
+    setState(() {
+      obscureText = false;
+    });
+  }
+
+  void closeObscureText() {
+    setState(() {
+      obscureText = true;
+    });
+  }
+
+  bool validateUserName() {
+    if (userNameController.text.isEmpty || userNameController.text == null) {
+      setState(() {
+        borderUsernameColor = Colors.red;
+      });
+      return false;
+    } else {
+      setState(() {
+        borderUsernameColor = Colors.white;
+      });
+      return null;
+    }
+  }
+
+  bool validatePassword() {
+    if (passwordController.text.isEmpty || passwordController.text == null) {
+      setState(() {
+        borderPasswordColor = Colors.red;
+      });
+      return false;
+    } else {
+      setState(() {
+        borderPasswordColor = Colors.white;
+      });
+      return null;
+    }
+  }
+
+  bool validate() {
+    bool isValid = true;
+    isValid = validateUserName() ?? isValid;
+    isValid = validatePassword() ?? isValid;
+    return isValid;
+  }
+
+  void startLoading() {
+    setState(() {
+      onLoading = true;
+    });
+  }
+
+  void stopLoading() {
+    setState(() {
+      onLoading = false;
+    });
+  }
+
+  Future<void> safeUser(UserModel user) async {
+    await prefs.setString(PrefsKey.user, json.encode(user.toJson()));
+    return;
+  }
+
+  void submit() {
+    if (!validate()) return;
+    startLoading();
+    UserModel.login(
+      password: passwordController.text,
+      username: userNameController.text,
+    ).then((onValue) {
+      stopLoading();
+      safeUser(onValue).then((res) {
+        String _user = (prefs.getString(PrefsKey.user));
+        print(_user);
+        return showModalBottomSheet(
+          context: context,
+          builder: (BuildContext context) {
+            return Container(
+              height: 100,
+              color: Colors.white,
+              child: Center(
+                child: Text("Selamat Datang ${onValue.name}"),
+              ),
+            );
+          },
+        );
+      });
+    }).catchError((onError) {
+      stopLoading();
+      return showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            height: 100,
+            color: Colors.white,
+            child: Center(
+              child: Text("login gagal $onError"),
+            ),
+          );
+        },
+      );
+    });
+  }
+}
---
+++ lib/view/LoginView.dart
@@ -0,0 +1,201 @@
+import 'package:flutter/material.dart';
+import 'package:pelatihan_dasar_flutter/presenter/LoginPresenter.dart';
+
+class LoginView extends LoginPresenter {
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        colors: <Color>[
+          Color(0xff00549E),
+          Color(0xff008AF6),
+        ],
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+      )),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: <Widget>[
+            Container(
+              height: double.infinity,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    SafeArea(
+                      child: Container(
+                        height: 200,
+                        width: double.infinity,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Image.asset("assets/logo_suzuki.png"),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              user != null ? "Selamat datang kembali ${user.name}" : "",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 30, right: 30),
+                      height: 40,
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            height: 40,
+                            width: 40,
+                            margin: EdgeInsets.only(right: 10),
+                            child: Image.asset(
+                              "assets/icon_user.png",
+                              alignment: Alignment.bottomCenter,
+                            ),
+                          ),
+                          Expanded(
+                            child: TextField(
+                              controller: userNameController,
+                              cursorColor: Colors.white,
+                              focusNode: usernameFocusNode,
+                              onSubmitted: (res) {
+                                FocusScope.of(context)
+                                    .requestFocus(passwordFocusNode);
+                              },
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                              decoration: InputDecoration(
+                                focusColor: Colors.white,
+                                hintText: "Nama Pengguna",
+                                hintStyle: TextStyle(
+                                  color: Colors.white30,
+                                ),
+                                enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                  color: borderUsernameColor,
+                                )),
+                                focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                  color: borderUsernameColor,
+                                )),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 30, right: 30),
+                      height: 40,
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            height: 40,
+                            width: 40,
+                            margin: EdgeInsets.only(right: 10),
+                            child: Image.asset(
+                              "assets/icon_keys.png",
+                              alignment: Alignment.bottomCenter,
+                            ),
+                          ),
+                          Expanded(
+                              child: TextField(
+                            controller: passwordController,
+                            cursorColor: Colors.white,
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                            focusNode: passwordFocusNode,
+                            obscureText: obscureText,
+                            decoration: InputDecoration(
+                                focusColor: Colors.white,
+                                hintText: "Nama Pengguna",
+                                hintStyle: TextStyle(
+                                  color: Colors.white30,
+                                ),
+                                enabledBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                  color: borderPasswordColor,
+                                )),
+                                focusedBorder: UnderlineInputBorder(
+                                    borderSide: BorderSide(
+                                  color: borderPasswordColor,
+                                )),
+                                suffixIcon: GestureDetector(
+                                  onLongPressStart: (a) {
+                                    openObscureText();
+                                  },
+                                  onLongPressEnd: (a) {
+                                    closeObscureText();
+                                  },
+                                  child: Container(
+                                    width: 20,
+                                    child: Image.asset(
+                                      "assets/icon_close_eyes.png",
+                                      alignment: Alignment.center,
+                                    ),
+                                  ),
+                                )),
+                          ))
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    GestureDetector(
+                      onTap: submit,
+                      child: Container(
+                        margin: EdgeInsets.only(left: 30, right: 30),
+                        height: 50,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            color: Color(0xff008AF6),
+                            borderRadius: BorderRadius.all(Radius.circular(5))),
+                        child: Center(
+                            child: Text(
+                          "Masuk",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        )),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            onLoading == true
+                ? Container(
+                    color: Colors.white.withOpacity(0.1),
+                    padding: EdgeInsets.only(bottom: 50),
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: CircularProgressIndicator(
+                        valueColor:
+                            new AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  )
+                : Container(
+                    height: 0,
+                    width: 0,
+                  )
+          ],
+        ),
+      ),
+    );
+  }
+}
